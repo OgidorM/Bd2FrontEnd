f4d96cef5cdd5222d925abfdf3a3952e405347c1
OgidorM <151182218+OgidorM@users.noreply.github.com>
2025-11-12 22:46:16 +0000

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,96 @@
+import React, {useRef} from "react";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import {SplitText} from "gsap/all";
+import {useMediaQuery} from "react-responsive";
+
+const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', {type: 'chars, words'});
+        const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.12,
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? 'top 120%' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin:true,
+            }
+        })
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
+    }, []);
+    return (
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">Cinetugal</h1>
+
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool.  Crisp. Classic.</p>
+                            <p className="subtitle">
+                                Sip the Spirit <br /> of Summer
+                            </p>
+                        </div>
+
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes -designed to delight your senses.
+                            </p>
+                            <a href="/cocktails">View Cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/popcorn3.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
+        </>
+    )
+}
+export default Hero;
